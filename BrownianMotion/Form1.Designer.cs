cfe62cd18a9a4c6fddc95ebcb72de482a2a5ea53
123DMWM <shmo1joe2@gmail.com>
2015-06-14 16:44:33 -0400

    Initial Commit

---
+++ BrownianMotion/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace BrownianMotion {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.Location = new System.Drawing.Point(13, 38);
+			this.pictureBox1.MinimumSize = new System.Drawing.Size(625, 625);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(625, 625);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(58, 12);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
+			this.numericUpDown1.TabIndex = 1;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            666,
+            0,
+            0,
+            0});
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(189, 9);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Generate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(40, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Length";
+			// 
+			// backgroundWorker1
+			// 
+			this.backgroundWorker1.WorkerReportsProgress = true;
+			this.backgroundWorker1.WorkerSupportsCancellation = true;
+			this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+			this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+			this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.progressBar1.Location = new System.Drawing.Point(374, 9);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(264, 23);
+			this.progressBar1.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(313, 14);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(9, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "|";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(270, 9);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(37, 23);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "New";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Location = new System.Drawing.Point(119, 14);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(64, 17);
+			this.checkBox1.TabIndex = 7;
+			this.checkBox1.Text = "Watch?";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(650, 677);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.pictureBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MinimumSize = new System.Drawing.Size(666, 715);
+			this.Name = "Form1";
+			this.Text = "BrownianMotion";
+			this.ResizeEnd += new System.EventHandler(this.form1_ResizeEnd);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.CheckBox checkBox1;
+	}
+}
+
---
+++ BrownianMotion/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrownianMotion {
+	public partial class Form1 : Form {
+		Bitmap updatedImage;
+		int crop = 1280;
+		public Form1() {
+			InitializeComponent();
+		}
+
+		//Start line generator
+		private void button1_Click(object sender, EventArgs e) {
+			if (!backgroundWorker1.IsBusy) {
+				button1.Text = "Cancel";
+				button2.Enabled = false;
+				numericUpDown1.Enabled = false;
+				if (pictureBox1.Image == null) {
+					resetPictureBox();
+				}
+				backgroundWorker1.RunWorkerAsync();
+			} else if (backgroundWorker1.WorkerSupportsCancellation) {
+				backgroundWorker1.CancelAsync();
+			}
+
+		}
+
+		//reset pictureBox1 to white rectangle
+		private void button2_Click(object sender, EventArgs e) {
+			resetPictureBox();
+		}
+
+		//Fills in pictureBox1 with white rectangle
+		private void resetPictureBox() {
+			Bitmap Bmp = new Bitmap(2561, 2561);
+			Graphics g = Graphics.FromImage(Bmp);
+			g.FillRectangle(new SolidBrush(Color.White), 0, 0, 2561, 2561);
+			crop = 1280;
+			setImage(Bmp);
+		}
+		//render image in pictureBox1
+		private void setImage(Bitmap img) {
+			double? widthScale = pictureBox1.Width / (double)img.Width;
+			double? heightScale = pictureBox1.Height / (double)img.Height;
+			double scale = Math.Min((double)(widthScale ?? heightScale),
+									 (double)(heightScale ?? widthScale));
+			var bmp2 = new Bitmap((int)Math.Floor(img.Width * scale), (int)Math.Ceiling(img.Height * scale));
+			Graphics g = Graphics.FromImage(bmp2);
+			g.InterpolationMode = InterpolationMode.NearestNeighbor;
+			g.DrawImage(cropAtRect(img, new Rectangle(crop, crop, 2561 - crop * 2, 2561 - crop * 2)), new Rectangle(0, 0, bmp2.Size.Width, bmp2.Size.Height));
+			pictureBox1.Image = bmp2;
+			updatedImage = img;
+		}
+		//crops the image to where the generated stuff is
+		private Bitmap cropAtRect(Bitmap b, Rectangle r) {
+			Bitmap nb = new Bitmap(r.Width, r.Height);
+			Graphics g = Graphics.FromImage(nb);
+			g.DrawImage(b, -r.X, -r.Y);
+			return nb;
+		}
+		private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
+			BackgroundWorker worker = sender as BackgroundWorker;
+			int center = 1280, x = 0, y = 0;
+			Bitmap image = updatedImage;
+			Random random;
+			try {
+				Graphics g = Graphics.FromImage(image);
+				for (int i = 0; i < numericUpDown1.Value; i++) {
+					random = new Random(Guid.NewGuid().GetHashCode());
+					string[] hexRandom = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+					string randomHex = "#FF";
+					for (int b = 0; b < 6; b++) {
+						randomHex += hexRandom[random.Next(0, hexRandom.Length)];
+					}
+					int argb = Int32.Parse(randomHex.Replace("#", ""), NumberStyles.HexNumber);
+					x = putInside(x + random.Next(-1, 2));
+					y = putInside(y + random.Next(-1, 2));
+					int min = Math.Min(Math.Min(center - x, center + x), Math.Min(center + y, center - y));
+					crop = ((min < crop) ? min : crop);
+					//draw line and duplicates
+					g.FillRectangle(new SolidBrush(Color.FromArgb(argb)), center + x, center + y, 1, 1);
+					g.FillRectangle(new SolidBrush(Color.FromArgb(argb)), center + x, center - y, 1, 1);
+					g.FillRectangle(new SolidBrush(Color.FromArgb(argb)), center - x, center + y, 1, 1);
+					g.FillRectangle(new SolidBrush(Color.FromArgb(argb)), center - x, center - y, 1, 1);
+
+					g.FillRectangle(new SolidBrush(Color.FromArgb(argb)), center + y, center + x, 1, 1);
+					g.FillRectangle(new SolidBrush(Color.FromArgb(argb)), center - y, center + x, 1, 1);
+					g.FillRectangle(new SolidBrush(Color.FromArgb(argb)), center + y, center - x, 1, 1);
+					g.FillRectangle(new SolidBrush(Color.FromArgb(argb)), center - y, center - x, 1, 1);
+					if (checkBox1.Checked) {
+						setImage(image);
+					}
+					worker.ReportProgress(((i / (int)numericUpDown1.Value) * 100));
+				}
+				if (!checkBox1.Checked) {
+					setImage(image);
+				}
+			} catch (Exception ex) {
+				MessageBox.Show(ex.ToString(),
+				"Error generating image.",
+				MessageBoxButtons.OK,
+				MessageBoxIcon.Error);
+			}
+		}
+		//makes sure coords are inside the image
+		private int putInside(int coord) {
+			if (1280 + coord >= 2561 || 1280 - coord >= 2561) {
+				return 2561;
+			} else if (1280 + coord < 0 || 1280 - coord < 0) {
+				return 0;
+			}
+			return coord;
+		}
+
+		private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+			progressBar1.Value = e.ProgressPercentage;
+			label2.Text = e.ProgressPercentage + "%";
+		}
+		private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+			if (e.Cancelled == true) {
+				label2.Text = "Canceled!";
+			} else if (e.Error != null) {
+				label2.Text = "Error: " + e.Error.Message;
+			} else {
+				label2.Text = "Done!";
+			}
+			button1.Text = "Generate";
+			button2.Enabled = true;
+			numericUpDown1.Enabled = true;
+			progressBar1.Value = 0;
+		}
+		//resize image when form size changes
+		private void form1_ResizeEnd(object sender, EventArgs e) {
+			if (!backgroundWorker1.IsBusy) {
+				setImage(updatedImage);
+			}
+		}
+	}
+}
